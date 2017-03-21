6390813df15d13558c57cb1ebb3dbfe01f19f5d4
ortalo <pierre.ortalo@gmail.com>
2017-03-21 13:18:04 +0100

    Add files via upload
    Version avec multiThreading

--- airMolIP.py
+++ airMolIP.py
@@ -3,23 +3,37 @@ import tkMessageBox
 import tkSimpleDialog
 from Tkinter import *
 from pymol import cmd
+import time
+from threading import *
 
-class IPserver:
+class IPserver(Thread):
+
     def __init__(self):
-        self.server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.IP=socket.gethostbyname(socket.gethostname())
+        Thread.__init__(self)
+        self.server_socket=socket.socket()
+        self.IP=([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+
         self.client_socket=socket.socket()
         self.infos=""
         self.connected=False
-
+    
     def start_server(self):
-	self.server_socket.bind(("",0))
+        self.server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	self.server_socket.bind(("",8888))
         self.server_socket.listen(1)
         self.client_socket,self.infos=self.server_socket.accept()
         self.connected=True
         print "connected"
+        while self.connected: 
+            data= self.client_socket.recv(2048)
+            print(data.decode())
+            if data=="fin":
+                self.client_socket.close()
+                self.connected=False
+                self.server_socket.close()
 
-        data= self.client_socket.recv(2048)
+    def run(self):
+        self.start_server()
 
 def __init__(self):
 	self.menuBar.addmenuitem('Plugin', 'command',
@@ -29,13 +43,12 @@ def __init__(self):
 	
 def airMol(self):
     server=IPserver()
+    server.start()
     root = Tk()
     root.title("AirMol")
     root.geometry("300x200")
     Label(root, text="        Local IP Address: ").grid(row=0,column=1,sticky=W)
-    L = Label(root, text=([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]))
-    
-    
+    L = Label(root, text=server.IP)
     L.grid(row=0,column=2,sticky=W)
     
     Label(root, text="        Connected:").grid(row=4,column=1,sticky=W)
@@ -43,17 +56,15 @@ def airMol(self):
     label.grid(row=4,column=2,sticky=W)
     label.rowconfigure(0, pad=3)
     
-    def update():
+    def checkCon():
         if server.connected==True:
-            label.configure(text="yes")
-        root.after(1000, update)
+            label=Label(root,text="yes")
+        else:
+            label=Label(root,text="no")
+        root.after(1000, checkCon)
 
-    root.after(1000,update)
+    root.after(0,checkCon)
     
-    window = tkMessageBox.showinfo('AirMol',
-                                      'Your IP adress: '+([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])+"\n Use it to connect from the AirMol App",
-                                      parent=self.root)
-
 cmd.extend('airMol', airMol)
 
 
